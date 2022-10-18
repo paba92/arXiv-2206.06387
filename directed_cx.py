c96670a325a46c289cffaf84d85197dba5e77054
Matthias Zipper <matthiaszipper@gmail.com>
2022-10-18 18:28:59 +0200

    Improved Alg. 2; added utilities for testing and showcasing

--- directed_cx.py
+++ directed_cx.py
@@ -3,6 +3,8 @@ from typing import Union
 
 import numpy as np
 
+from qiskit import QuantumCircuit
+
 
 def default_h_layout(n: int) -> np.ndarray:
     # Prepare default Hadamard layout (Eq. 31)
@@ -54,46 +56,96 @@ def algorithm_1(n: int, t_cz: np.ndarray, return_list: bool = False) -> Union[np
         return t_h
 
 
-def algorithm_2(n: int, t_h: np.ndarray, t_cz: np.ndarray):
+def algorithm_2(n: int, t_h: np.ndarray, t_cz: np.ndarray) -> tuple[np.ndarray, list[np.ndarray]]:
     ## Algorithm 2
     #
     # Generate the CZ/GCZ
-    ZZ = []
-    CZ = np.empty((n, 0))
-    flag = False
+    seq = []
+    cur_gcz = []
     for i in range(n-1):
         if sum(t_h[:, i]) > 0:  # There is at least one hadamard gate at column i
             if sum(t_h[:, i+1]) > 0:  # If the ith column is surrounded by hadamard layers.
-                if flag:  # If CZ is non empty, then append the next column of t_cz to CZ
+                if cur_gcz:  # If CZ is non empty, then append the next column of t_cz to CZ
                     if sum(t_cz[:, i]) > 1:
-                        CZ = np.c_[CZ, t_cz[:, i]]
-                    ZZ.append(CZ)  # Add CZ to the sequence of global CZ gates
-                    CZ = np.empty((n, 0))
-                    flag = False
+                        cur_gcz.append(t_cz[:, i])
+                    seq.append(cur_gcz)  # Add CZ to the sequence of global CZ gates
+                    cur_gcz = []
                 else:  # CZ is empty
-                    CZ = np.reshape(np.copy(t_h[:, i+1]), (t_h.shape[0], 1))
-                    CZ[i, 0] = 1
-                    ZZ.append(CZ)  # Append a two qubit CZ (first part of the column of t_cz)
-                    CZ = (t_cz[:, i] + t_h[:, i+1]) % 2  # The second part of the column of t_cz
-                    if sum(CZ) < 2:
-                        CZ = np.empty((n, 0))
-                        flag = False
+                    twoq_cz = np.copy(t_h[:, i+1])
+                    twoq_cz[i] = 1
+                    seq.append(twoq_cz.reshape((n, 1)))  # Append a two qubit CZ (first part of the column of t_cz)
+                    remainder_cz = (t_cz[:, i] + t_h[:, i+1]) % 2  # The second part of the column of t_cz
+                    if sum(remainder_cz) < 2:
+                        cur_gcz = []
                     else:
-                        CZ = np.reshape(CZ, (CZ.shape[0], 1))
-                        flag = True
+                        cur_gcz = [remainder_cz]
             else:
                 if sum(t_cz[:, i]) > 1:
-                    CZ = np.c_[CZ, t_cz[:, i]]
-                ZZ.append(CZ)
-                CZ = np.empty((n, 0))
-                flag = False
+                    cur_gcz.append(t_cz[:, i])
+                seq.append(cur_gcz)  # TODO: This may append empty GCZ layers to the sequence!
+                cur_gcz = []
         else:
             if sum(t_cz[:, i]) > 1:
-                ZZ[-1] = np.c_[ZZ[-1], t_cz[:, i]]
-    t_h = np.delete(t_h, np.argwhere(np.all(t_h[..., :] == 0, axis=0)), axis=1)
+                seq[-1].append(t_cz[:, i])
     # RETURN
-    return t_h, ZZ
+    return (
+        np.delete(t_h, np.argwhere(np.all(t_h[..., :] == 0, axis=0)), axis=1),  # truncated H pattern
+        [(np.column_stack(lay)  # assemble list of fan-out-like gates to GCZ layer matrix
+          if isinstance(lay, list) else
+          lay)  # two-qubit CZs are already in the desired format
+         for lay in seq]  # sequence of (two-qubit) CZ and GCZ layers
+    )
 
 
 # def directed_cx_to_h_gcz(n: int, b: np.ndarray) -> list[list[Union[int, tuple[int, int]]]]:
 #     pass
+
+
+def create_random_directed_cx(n: int):
+    t_cz = np.tril(np.random.randint(0, 2, (n, n), np.uint8), -1)  # populate strictly lower triangle at random
+    for j in range(n-1):
+        if sum(t_cz[j+1:, j]):
+            t_cz[j, j] = 1  # set control on diagonal, if any target has been randomly selected in that column
+    return t_cz
+
+
+def to_qiskit(n: int, t_h, seq):
+    assert t_h.shape[1] == len(seq) + 1  # TODO: Does that always hold?
+    qc = QuantumCircuit(n)
+    for h_lay, cz_lay in zip(t_h.T, seq):
+        for i in np.flatnonzero(h_lay):
+            qc.h(i)
+        qc.barrier()
+        for fanout in cz_lay.T:
+            idcs = np.flatnonzero(fanout)
+            ctrl = idcs[0]
+            for trgt in idcs[1:]:
+                qc.cz(ctrl, trgt)
+        qc.barrier()
+    for i in np.flatnonzero(t_h[:, -1]):
+        qc.h(i)
+    return qc
+
+
+if __name__ == '__main__':
+    n = 6
+
+    t_cz = create_random_directed_cx(n)
+    print("Random directed CX layer layout:")
+    print(t_cz)
+
+    t_h = algorithm_1(n, t_cz)
+    print("Corresponding modified H pattern (post-Alg1):")
+    print(t_h)
+    print()
+
+    red_t_h, seq = algorithm_2(n, t_h, t_cz)
+    print("Reduced H pattern after GCZ formation (post-Alg2):")
+    print(red_t_h)
+    print("Diagonal gate sequence:")
+    for i, lay in enumerate(seq):
+        print(f"-> layer {i}:")
+        print(lay)
+
+    qc = to_qiskit(n, red_t_h, seq)
+    print(qc)
